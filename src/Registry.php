9f4cbed375356b0e2f607d52f220f3da2e39c017
Jason McCreary <jason@pureconcepts.net>
2022-02-25 08:02:34 -0500

    Add list of core Symfony packages

--- src/Registry.php
+++ src/Registry.php
@@ -61,6 +61,28 @@ class Registry
     }
 
     /**
+     * Returns the constraint used by the specified Laravel
+     * version for the included Symfony components.
+     *
+     * @param string $version 'latest', '8.x', '7.x', '6.x'
+     * @return string
+     */
+    public static function symfonyConstraintFor(string $version): string
+    {
+        static $constraints = [
+            'latest' => '^6.0',
+            '9.x' => '^6.0',
+            '8.x' => '^5.1.4',
+            '7.x' => '^5.0',
+            '6.x' => '^4.3.4',
+        ];
+
+        self::verifyLaravelVersion($version);
+
+        return $constraints[$version];
+    }
+
+    /**
      * Returns a list of all Illuminate packages.
      *
      * @return array
@@ -101,25 +123,133 @@ class Registry
     }
 
     /**
-     * Returns the constraint used by the specified Laravel
-     * version for the included Symfony components.
+     * Returns a list of all core Symfony packages.
      *
-     * @param string $version 'latest', '8.x', '7.x', '6.x'
-     * @return string
+     * @return array
      */
-    public static function symfonyConstraintFor(string $version): string
+    public static function symfonyPackages(): array
     {
-        static $constraints = [
-            'latest' => '^6.0',
-            '9.x' => '^6.0',
-            '8.x' => '^5.1.4',
-            '7.x' => '^5.0',
-            '6.x' => '^4.3.4',
+        return [
+            'symfony/all-my-sms-notifier',
+            'symfony/amazon-mailer',
+            'symfony/amazon-sns-notifier',
+            'symfony/amazon-sqs-messenger',
+            'symfony/amqp-messenger',
+            'symfony/asset',
+            'symfony/beanstalkd-messenger',
+            'symfony/browser-kit',
+            'symfony/cache',
+            'symfony/clickatell-notifier',
+            'symfony/config',
+            'symfony/console',
+            'symfony/crowdin-translation-provider',
+            'symfony/css-selector',
+            'symfony/debug-bundle',
+            'symfony/dependency-injection',
+            'symfony/discord-notifier',
+            'symfony/doctrine-bridge',
+            'symfony/doctrine-messenger',
+            'symfony/dom-crawler',
+            'symfony/dotenv',
+            'symfony/error-handler',
+            'symfony/esendex-notifier',
+            'symfony/event-dispatcher',
+            'symfony/expo-notifier',
+            'symfony/expression-language',
+            'symfony/fake-chat-notifier',
+            'symfony/fake-sms-notifier',
+            'symfony/filesystem',
+            'symfony/finder',
+            'symfony/firebase-notifier',
+            'symfony/form',
+            'symfony/framework-bundle',
+            'symfony/free-mobile-notifier',
+            'symfony/gateway-api-notifier',
+            'symfony/gitter-notifier',
+            'symfony/google-chat-notifier',
+            'symfony/google-mailer',
+            'symfony/http-client',
+            'symfony/http-foundation',
+            'symfony/http-kernel',
+            'symfony/infobip-notifier',
+            'symfony/intl',
+            'symfony/iqsms-notifier',
+            'symfony/ldap',
+            'symfony/light-sms-notifier',
+            'symfony/linked-in-notifier',
+            'symfony/lock',
+            'symfony/loco-translation-provider',
+            'symfony/lokalise-translation-provider',
+            'symfony/mailchimp-mailer',
+            'symfony/mailer',
+            'symfony/mailgun-mailer',
+            'symfony/mailjet-mailer',
+            'symfony/mailjet-notifier',
+            'symfony/mattermost-notifier',
+            'symfony/mercure-notifier',
+            'symfony/message-bird-notifier',
+            'symfony/message-media-notifier',
+            'symfony/messenger',
+            'symfony/microsoft-teams-notifier',
+            'symfony/mime',
+            'symfony/mobyt-notifier',
+            'symfony/monolog-bridge',
+            'symfony/notifier',
+            'symfony/octopush-notifier',
+            'symfony/oh-my-smtp-mailer',
+            'symfony/one-signal-notifier',
+            'symfony/options-resolver',
+            'symfony/ovh-cloud-notifier',
+            'symfony/password-hasher',
+            'symfony/postmark-mailer',
+            'symfony/process',
+            'symfony/property-access',
+            'symfony/property-info',
+            'symfony/proxy-manager-bridge',
+            'symfony/rate-limiter',
+            'symfony/redis-messenger',
+            'symfony/rocket-chat-notifier',
+            'symfony/routing',
+            'symfony/runtime',
+            'symfony/security',
+            'symfony/security-bundle',
+            'symfony/security-core',
+            'symfony/security-csrf',
+            'symfony/security-http',
+            'symfony/semaphore',
+            'symfony/sendgrid-mailer',
+            'symfony/sendinblue-mailer',
+            'symfony/sendinblue-notifier',
+            'symfony/serializer',
+            'symfony/sinch-notifier',
+            'symfony/slack-notifier',
+            'symfony/sms-biuras-notifier',
+            'symfony/sms77-notifier',
+            'symfony/smsapi-notifier',
+            'symfony/smsc-notifier',
+            'symfony/spot-hit-notifier',
+            'symfony/stopwatch',
+            'symfony/string',
+            'symfony/telegram-notifier',
+            'symfony/telnyx-notifier',
+            'symfony/templating',
+            'symfony/translation',
+            'symfony/turbo-sms-notifier',
+            'symfony/twig-bridge',
+            'symfony/twig-bundle',
+            'symfony/twilio-notifier',
+            'symfony/uid',
+            'symfony/validator',
+            'symfony/var-dumper',
+            'symfony/var-exporter',
+            'symfony/vonage-notifier',
+            'symfony/web-link',
+            'symfony/web-profiler-bundle',
+            'symfony/workflow',
+            'symfony/yaml',
+            'symfony/yunpian-notifier',
+            'symfony/zulip-notifier',
         ];
-
-        self::verifyLaravelVersion($version);
-
-        return $constraints[$version];
     }
 
     private static function corePackages(string $version)
